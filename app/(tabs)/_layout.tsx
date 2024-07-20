72780ef28ce6fe76853a6d4d3cca09586f0408bf
ermanos <herman226.r@gmail.com>
2024-07-20 17:23:57 +0300

    tabs-icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,42 @@
 import React from "react";
 import { Tabs } from "expo-router";
+import { FontAwesome } from "@expo/vector-icons";
+import Colors from "constants/Colors";
+
+const TabBarIcon = (props: {
+  name: React.ComponentProps<typeof FontAwesome>["name"];
+  color: string;
+}) => <FontAwesome size={20} {...props} />;
 
 const TabsLayout = () => {
   return (
     <Tabs>
-      <Tabs.Screen name="profile" options={{ title: "Profile" }} />
-      <Tabs.Screen name="map" options={{ title: "Map" }} />
-      <Tabs.Screen name="events" options={{ title: "Events" }} />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ color }) => <TabBarIcon name="user" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="map"
+        options={{
+          title: "Map",
+          headerShown: false,
+          tabBarIcon: ({ color }) => <TabBarIcon name="fire" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="events"
+        options={{
+          title: "Events",
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="calendar" color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 };
