305c0fdd14a8be80841cb673ff95c34247c3f7c5
ermanos <herman226.r@gmail.com>
2024-07-26 18:13:26 +0300

    login-formik-validation

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,45 +1,87 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
 import { router } from 'expo-router';
 import { useAppDispatch, useAppSelector } from 'features/hooks';
 import { login } from '@store/user/user.thunk';
 import { Button, TextInput } from 'react-native-paper';
+import { Formik } from 'formik';
+import * as z from 'zod';
+import { toFormikValidationSchema } from 'zod-formik-adapter';
+
+const loginSchema = z.object({
+  email: z.string().email('Invalid email address'),
+  password: z.string().min(6, 'Password must be at least 6 characters long'),
+});
 
 const Login = () => {
   const dispatch = useAppDispatch();
   const { loading } = useAppSelector((state) => state.user);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+
+  const initialValues = {
+    email: '',
+    password: '',
+  };
+
+  const handleLogin = (values: typeof initialValues) => {
+    dispatch(login(values));
+  };
 
   return (
-    <View style={styles.container}>
-      <TextInput
-        label="Email"
-        value={email}
-        autoCapitalize="none"
-        onChangeText={(text) => setEmail(text)}
-        mode="outlined"
-      ></TextInput>
-      <TextInput
-        label="Password"
-        value={password}
-        secureTextEntry
-        autoCapitalize="none"
-        onChangeText={(text) => setPassword(text)}
-        mode="outlined"
-      ></TextInput>
-      <Button
-        onPress={() => dispatch(login({ email, password }))}
-        mode="contained"
-        loading={loading.login}
-        disabled={loading.login}
-      >
-        Login
-      </Button>
-      <Button onPress={() => router.push('/(auth)/signup')} mode="outlined">
-        Create account
-      </Button>
-    </View>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={toFormikValidationSchema(loginSchema)}
+      onSubmit={handleLogin}
+    >
+      {({
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        values,
+        errors,
+        touched,
+      }) => (
+        <View style={styles.container}>
+          <TextInput
+            label="Email"
+            value={values.email}
+            style={styles.input}
+            autoCapitalize="none"
+            onChangeText={handleChange('email')}
+            onBlur={handleBlur('email')}
+            mode="outlined"
+            error={touched.email && !!errors.email}
+          />
+          {touched.email && errors.email && (
+            <Text style={styles.errorText}>{errors.email}</Text>
+          )}
+          <TextInput
+            label="Password"
+            value={values.password}
+            style={styles.input}
+            secureTextEntry
+            autoCapitalize="none"
+            onChangeText={handleChange('password')}
+            onBlur={handleBlur('password')}
+            mode="outlined"
+            error={touched.password && !!errors.password}
+          />
+          {touched.password && errors.password && (
+            <Text style={styles.errorText}>{errors.password}</Text>
+          )}
+          <Button
+            onPress={() => handleSubmit()}
+            mode="contained"
+            loading={loading.login}
+            disabled={loading.login}
+          >
+            Login
+          </Button>
+          <Button onPress={() => router.push('/(auth)/signup')} mode="outlined">
+            Create account
+          </Button>
+        </View>
+      )}
+    </Formik>
   );
 };
 
@@ -49,7 +91,12 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     display: 'flex',
-    gap: 20,
+  },
+  input: {},
+  errorText: {
+    color: 'red',
+    fontSize: 12,
+    marginBottom: 10,
   },
 });
 
