104dd1a95ab246898b55f41b521102d6fbfb5b54
psolas <psolas@psoliou.com>
2025-02-12 22:43:31 +0200

    remove-index-pages

--- app/(tabs)/events/index.tsx
+++ app/(tabs)/events/index.tsx
@@ -1,3 +1,71 @@
-import { Events } from './events';
+import { FlatList, StyleSheet, View } from 'react-native'
+import React, { useEffect, useMemo } from 'react'
+import { useAppDispatch, useAppSelector } from 'features/hooks'
+import EventListItem from '@components/Events/EventListItem/EventListItem'
+import { fetchEvents } from '@store/events/events.thunk'
+import { FAB } from 'react-native-paper'
+import { router } from 'expo-router'
 
-export default Events;
+const Events = () => {
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(fetchEvents())
+  }, [])
+
+  const {
+    events,
+    loading: { fetchingEvents }
+  } = useAppSelector((state) => state.eventsSlice)
+
+  const { appUser } = useAppSelector((state) => state.userSlice)
+
+  // sort emergencies on top
+  const sortedEvents = useMemo(() => {
+    return events
+      ? [...events].sort((a, b) => {
+          if (a.type === 'EMERGENCY' && b.type !== 'EMERGENCY') {
+            return -1
+          }
+          if (a.type !== 'EMERGENCY' && b.type === 'EMERGENCY') {
+            return 1
+          }
+          return 0
+        })
+      : []
+  }, [events])
+
+  return (
+    <View style={styles.container}>
+      {!!events.length && (
+        <FlatList
+          data={sortedEvents}
+          renderItem={({ item }) => (
+            <EventListItem event={item} key={item.id} />
+          )}
+          onRefresh={() => dispatch(fetchEvents())}
+          refreshing={fetchingEvents}
+        />
+      )}
+      {appUser?.isAdmin && !fetchingEvents && (
+        <FAB
+          icon="plus"
+          style={styles.FAB}
+          onPress={() => router.push('/events/new-event')}
+        />
+      )}
+    </View>
+  )
+}
+
+export default Events
+
+const styles = StyleSheet.create({
+  container: {},
+  FAB: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    margin: 0
+  }
+})
--- app/(tabs)/shifts/index.tsx
+++ app/(tabs)/shifts/index.tsx
@@ -1,3 +1,63 @@
-import { ShiftsScreen } from './shifts';
+import { FlatList, StyleSheet, View } from 'react-native'
+import { useAppDispatch, useAppSelector } from 'features/hooks'
+import { FAB, Card, Text, Button } from 'react-native-paper'
+import { router } from 'expo-router'
+import { useEffect } from 'react'
+import { fetchShifts } from '@store/shifts/shifts.thunk'
 
-export default ShiftsScreen;
+const ShiftsScreen = () => {
+  const dispatch = useAppDispatch()
+  const { shifts, loading } = useAppSelector((state) => state.shiftsSlice)
+  const { appUser } = useAppSelector((state) => state.userSlice)
+
+  useEffect(() => {
+    dispatch(fetchShifts())
+  }, [])
+
+  return (
+    <View style={styles.container}>
+      safd
+      <FlatList
+        data={shifts}
+        renderItem={({ item }) => (
+          <Card
+            style={styles.card}
+            onPress={() => router.push(`/shifts/${item.id}`)}
+          >
+            <Card.Title
+              title={`${item.title}`}
+              subtitle={`Location: ${item.location.municipality}, ${item.location.province}`}
+            />
+            {/* <Card.Content>
+              <Text>
+                {item.reservedBy
+                  ? `Reserved by: ${item.reservedBy}`
+                  : 'Available'}
+              </Text>
+            </Card.Content> */}
+          </Card>
+        )}
+        onRefresh={() => dispatch(fetchShifts())}
+        refreshing={loading}
+      />
+      {appUser?.isAdmin && (
+        <FAB
+          icon="plus"
+          style={styles.fab}
+          onPress={() => router.push('/shifts/create')}
+        />
+      )}
+    </View>
+  )
+}
+
+export default ShiftsScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  segmentedButtons: {},
+  card: {},
+  fab: {}
+})
