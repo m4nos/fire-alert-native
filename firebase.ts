c07f679a17d54f85c8d5e29b8230690fb84a9a7a
ermanos <ermanos@orderitai.com>
2025-09-26 23:05:06 +0300

    upgrade-deps

--- firebase.ts
+++ firebase.ts
@@ -1,5 +1,4 @@
 import { initializeApp } from 'firebase/app'
-// @ts-expect-error https://stackoverflow.com/questions/76914913/cannot-import-getreactnativepersistence-in-firebase10-1-0
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth'
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage'
 import { getFirestore } from 'firebase/firestore'
--- metro.config.js
+++ metro.config.js
@@ -4,5 +4,6 @@ const { getDefaultConfig } = require('expo/metro-config')
 /** @type {import('expo/metro-config').MetroConfig} */
 const defaultConfig = getDefaultConfig(__dirname)
 defaultConfig.resolver.sourceExts.push('cjs')
+defaultConfig.resolver.unstable_enablePackageExports = false
 
 module.exports = defaultConfig
