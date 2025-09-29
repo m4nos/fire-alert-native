8e9e07f8f504cd2291e8d16c2e4c26278a8be2c1
ermanos <ermanos@orderitai.com>
2025-09-30 00:01:00 +0300

    configure-eas-build

--- firebase.ts
+++ firebase.ts
@@ -2,7 +2,7 @@ import { initializeApp } from 'firebase/app'
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth'
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage'
 import { getFirestore } from 'firebase/firestore'
-import { firebaseConfig } from 'firebase.config'
+import { firebaseConfig } from './firebase.config'
 
 const FirebaseApp = initializeApp(firebaseConfig)
 export const FirebaseAuth = initializeAuth(FirebaseApp, {
