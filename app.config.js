7c2db02ebb6543483424cb38dae7569b91321e25
ermanos <ermanos@orderitai.com>
2025-10-01 23:56:20 +0300

    use-dot-env-for-firebase-config

---
+++ app.config.js
@@ -0,0 +1,56 @@
+import 'dotenv/config'
+
+export default {
+  expo: {
+    name: 'Greece Fire Alert',
+    slug: 'greeceFirefighters4all',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/images/icon.png',
+    scheme: 'myapp',
+    userInterfaceStyle: 'automatic',
+    splash: {
+      image: './assets/images/splash.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff'
+    },
+    assetBundlePatterns: ['**/*'],
+    ios: {
+      supportsTablet: true
+    },
+    android: {
+      package: 'com.greecefirefighters4all',
+      versionCode: 1,
+      adaptiveIcon: {
+        foregroundImage: './assets/images/adaptive-icon.png',
+        backgroundColor: '#ffffff'
+      }
+    },
+    web: {
+      bundler: 'metro',
+      output: 'static',
+      favicon: './assets/images/favicon.png'
+    },
+    plugins: ['expo-router', 'expo-font'],
+    newArchEnabled: true,
+    experiments: {
+      typedRoutes: true,
+      tsconfigPaths: true
+    },
+    extra: {
+      router: {
+        origin: false
+      },
+      eas: {
+        projectId: 'df7318e4-55f9-4e22-8c41-fac0922ce02e'
+      },
+      FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+      FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+      FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+      FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+      FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+      FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID
+    }
+  }
+}
--- firebase.ts
+++ firebase.ts
@@ -2,7 +2,17 @@ import { initializeApp } from 'firebase/app'
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth'
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage'
 import { getFirestore } from 'firebase/firestore'
-import { firebaseConfig } from './firebase.config'
+import Constants from 'expo-constants'
+
+const firebaseConfig = {
+  apiKey: Constants.expoConfig?.extra?.FIREBASE_API_KEY,
+  authDomain: Constants.expoConfig?.extra?.FIREBASE_AUTH_DOMAIN,
+  projectId: Constants.expoConfig?.extra?.FIREBASE_PROJECT_ID,
+  storageBucket: Constants.expoConfig?.extra?.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: Constants.expoConfig?.extra?.FIREBASE_MESSAGING_SENDER_ID,
+  appId: Constants.expoConfig?.extra?.FIREBASE_APP_ID,
+  measurementId: Constants.expoConfig?.extra?.FIREBASE_MEASUREMENT_ID
+}
 
 const FirebaseApp = initializeApp(firebaseConfig)
 export const FirebaseAuth = initializeAuth(FirebaseApp, {
